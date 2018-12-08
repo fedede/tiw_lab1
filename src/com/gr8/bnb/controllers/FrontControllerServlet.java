4c58e0de70aff81869d0ac56a9fd99d478cc004a
Adri El Guapo <kugoad@openmailbox.org>
2018-12-08 18:16:38 +0100

    Updated Front Controller to use REST services

--- src/com/gr8/bnb/controllers/FrontControllerServlet.java
+++ src/com/gr8/bnb/controllers/FrontControllerServlet.java
@@ -1,7 +1,7 @@
 package com.gr8.bnb.controllers;
 
 
-import java.io.IOException;
+import java.io.IOException; 
 
 import javax.annotation.Resource;
 import javax.jms.ConnectionFactory;
@@ -16,33 +16,26 @@ import javax.servlet.http.HttpSession;
 import javax.transaction.NotSupportedException;
 import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
 
 import com.gr8.bnb.handlers.RequestHandler;
 import com.gr8.bnb.helpers.MessageManager;
+
 
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
+/**  
  * Servlet implementation class ControllerServlet
- */
-
+ */ 
+  
 public class FrontControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	@PersistenceContext(unitName="TIWbnb")
-	private EntityManager em;
-	
-	@Resource
-	private UserTransaction ut;
-	
-	@Resource(mappedName="tiwbnbcf")
-	private ConnectionFactory cf;
-	
-	@Resource(mappedName="tiwbnbqueue")
-	private Queue queue;
-
+	    
 	private ArrayList<String> noAuthPost = new ArrayList<String>();
 	private ArrayList<String> noAuthGet  = new ArrayList<String>();
 	
@@ -50,7 +43,7 @@ public class FrontControllerServlet extends HttpServlet {
 	
 	private final String HOME_JSP = "/index.jsp";
 	
-	private final String LOGIN_PAGE = "/login";
+	private final String LOGIN_PAGE = "/login"; 
 	private final String LOGOUT_PAGE = "/logout";
 	private final String SIGNUP_PAGE = "/signup";
 	private final String HOUSES_PAGE = "/house";
@@ -60,22 +53,22 @@ public class FrontControllerServlet extends HttpServlet {
 	private final String EDITPROFILE_PAGE = "/editprofile";
 	private final String PUBLISHHOUSE_PAGE = "/publishHouse";
 	
-
-	
-	
 	public void init() throws ServletException {
-		/* Create helper for sending and receiving messages */
-		MessageManager messageManager = new MessageManager(cf, queue);
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
 		
-		handlerHash.put(LOGIN_PAGE, new com.gr8.bnb.handlers.LoginHandler(em, ut));
-		handlerHash.put(LOGOUT_PAGE, new com.gr8.bnb.handlers.LogoutHandler(em, ut));
-		handlerHash.put(SIGNUP_PAGE, new com.gr8.bnb.handlers.SignupHandler(em, ut));
-		handlerHash.put(HOUSES_PAGE, new com.gr8.bnb.handlers.HouseHandler(em, ut, messageManager));
-		handlerHash.put(MESSAGES_PAGE, new com.gr8.bnb.handlers.MessagesHandler(em, ut, messageManager));
-		handlerHash.put(MESSAGE_PAGE, new com.gr8.bnb.handlers.MessageHandler(em, ut, messageManager));
-		handlerHash.put(RESULTS_PAGE, new com.gr8.bnb.handlers.ResultsHandler(em, ut));
-		handlerHash.put(EDITPROFILE_PAGE, new com.gr8.bnb.handlers.EditProfileHandler(em, ut));
-		handlerHash.put(PUBLISHHOUSE_PAGE, new com.gr8.bnb.handlers.HousePublishHandler(em, ut));
+		MessageManager messageManager = new MessageManager(client);
+		
+		/* Create helper for sending and receiving messages */
+		handlerHash.put(LOGIN_PAGE, new com.gr8.bnb.handlers.LoginHandler(client));
+		handlerHash.put(LOGOUT_PAGE, new com.gr8.bnb.handlers.LogoutHandler());
+		handlerHash.put(SIGNUP_PAGE, new com.gr8.bnb.handlers.SignupHandler(client));
+		handlerHash.put(HOUSES_PAGE, new com.gr8.bnb.handlers.HouseHandler(client));
+		handlerHash.put(MESSAGES_PAGE, new com.gr8.bnb.handlers.MessagesHandler(messageManager));
+		handlerHash.put(MESSAGE_PAGE, new com.gr8.bnb.handlers.MessageHandler(client, messageManager));
+		handlerHash.put(RESULTS_PAGE, new com.gr8.bnb.handlers.ResultsHandler(client));
+		handlerHash.put(EDITPROFILE_PAGE, new com.gr8.bnb.handlers.EditProfileHandler(client));
+		handlerHash.put(PUBLISHHOUSE_PAGE, new com.gr8.bnb.handlers.HousePublishHandler(client));
 		
 		noAuthGet.add(LOGIN_PAGE);
 		noAuthGet.add(SIGNUP_PAGE);
