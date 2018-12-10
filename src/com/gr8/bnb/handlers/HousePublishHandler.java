8a7fe5356647079e14ba694780b5e888023760e6
Adri El Guapo <kugoad@openmailbox.org>
2018-12-11 00:56:05 +0100

    Refactored HousePublishHandler to user InputChecker

--- src/com/gr8/bnb/handlers/HousePublishHandler.java
+++ src/com/gr8/bnb/handlers/HousePublishHandler.java
@@ -2,23 +2,14 @@ package com.gr8.bnb.handlers;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
@@ -26,11 +17,12 @@ import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.gr8.bnb.helpers.InputChecker;
+
 //import com.gr8.bnb.models.User;
 import model.User;
 //import com.gr8.bnb.helpers.InputChecker;
 import model.House;
-import model.Transaction;
 
 public class HousePublishHandler implements RequestHandler {
 
@@ -48,51 +40,29 @@ public class HousePublishHandler implements RequestHandler {
 	}
 
 	public String handlePost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NotSupportedException, SystemException {
-
+		String errorMessage = null;
 		
-		String houseName  = request.getParameter("inputHousename");
-		String fullDesc     = request.getParameter("inputFulldescription");
-		String shortDesc  = request.getParameter("inputShortdescription");
+		/* Get parameters. */
+		String houseName = request.getParameter("inputHousename");
+		String fullDescription = request.getParameter("inputFulldescription");
+		String shortDescription = request.getParameter("inputShortdescription");
 		String type = request.getParameter("inputHousetype");
 		String sPrice = request.getParameter("inputHouseprice");
-		String sGuestNum  = request.getParameter("inputGuestnumber");
-		String photo = request.getParameter("inputHousephoto");
+		String sMaxGuests  = request.getParameter("inputGuestnumber");
+		String photoURL = request.getParameter("inputHousephoto");
 		String city = request.getParameter("inputHousecity");
-		String sInitDate = request.getParameter("inputHousedatestart");
+		String sStartDate = request.getParameter("inputHousedatestart");
 		String sEndDate = request.getParameter("inputHousedateend");
-		
 		
 		HttpSession session = request.getSession();
 
 		/* Check if valid parameters an get error */
-		
-		String errorMessage = null;//InputChecker.checkUserParameters(name, surname, email, password);
-		
-		if(houseName == null || houseName.isEmpty()){
-			errorMessage = "Error in house name";
-		}else if(fullDesc == null ||fullDesc.isEmpty()){
-			errorMessage = "Error in full description";
-		}else if(shortDesc == null || shortDesc.isEmpty()){
-			errorMessage = "Error in short description";
-		}else if (type == null || type.isEmpty()){
-			errorMessage = "Error in type";
-		}else if(sGuestNum == null || sGuestNum.isEmpty()){
-			errorMessage = "Error in guest number2";
-		}else if(photo == null || photo.isEmpty()){
-			errorMessage = "Error in photo url";
-		}else if(city == null || city.isEmpty()){
-			errorMessage = "Error in city";
-		}else if(sPrice == null || sPrice.isEmpty()){
-			errorMessage = "Error in price1";
-		}else if(sInitDate == null || sInitDate.isEmpty()){
-			errorMessage = "Error in start date";
-		}else if(sEndDate == null || sEndDate.isEmpty()){
-			errorMessage = "Error in end date";
-		}
+		errorMessage = InputChecker.checkHouseParameters(houseName, fullDescription, 
+				shortDescription, type, sMaxGuests, photoURL, city, sPrice, sStartDate, sEndDate);
 		
 		int guestNum = 0;
 		try{
-			guestNum = Integer.parseInt(sGuestNum);
+			guestNum = Integer.parseInt(sMaxGuests);
 		}catch(Exception e){
 			guestNum = 0;
 		}
@@ -101,9 +71,9 @@ public class HousePublishHandler implements RequestHandler {
 			errorMessage = "Error in guest number2";
 		}
 		
-		int price = -1;
+		float price = -1;
 		try{
-			price = Integer.parseInt(sPrice);
+			price = Float.parseFloat(sPrice);
 		}catch(Exception e){
 			price = -1;
 		}
@@ -116,7 +86,7 @@ public class HousePublishHandler implements RequestHandler {
 		Date endDate;
 		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
 		try{
-			startDate = (Date) format.parse(sInitDate);
+			startDate = (Date) format.parse(sStartDate);
 			endDate = (Date) format.parse(sEndDate);
 		
 		}catch(java.text.ParseException e){
@@ -131,14 +101,16 @@ public class HousePublishHandler implements RequestHandler {
 			house.setStartDate(startDate);
 			house.setEndDate(endDate);
 			house.setName(houseName);
-			house.setFullDescription(fullDesc);
-			house.setShortDescription(shortDesc);
+			house.setFullDescription(fullDescription);
+			house.setShortDescription(shortDescription);
 			house.setCity(city.toUpperCase());
-			house.setImageUrl(photo);
+			house.setImageUrl(photoURL);
 			house.setMaxGuests(guestNum);
 			house.setShared(type.equals("shared"));
 			house.setPrice(price);
 			house.setOwner(owner);
+			
+			/* Perform post request to API. */
 			WebTarget housePath = houseWebTarget.path("house");
 			Builder builder = housePath.request(MediaType.APPLICATION_JSON);
 			
@@ -153,7 +125,7 @@ public class HousePublishHandler implements RequestHandler {
 		/* If error message send message to the view */
 		if (errorMessage != null) {
 			request.setAttribute("isHousePubPage", (Boolean) true);
-			request.setAttribute("housePubErrorMessage", errorMessage);
+			request.setAttribute("errorMessage", errorMessage);
 		}
 
 		/* Redirect to home page */
--- src/com/gr8/bnb/helpers/InputChecker.java
+++ src/com/gr8/bnb/helpers/InputChecker.java
@@ -5,6 +5,49 @@ public class InputChecker {
 	public static final String EMAIL_PATTERN = "^[\\w.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
 	
 	/**
+	 * Check if the input house parameters are valid in order to publish a new house.
+	 *
+	 * @param houseName Name of the user  house we want to register, error if blank.
+	 * @param fullDescription Full house description, error if blank.
+	 * @param shortDescription Short house description, error if blank.
+	 * @param type House type (shared or private), error if blank.
+	 * @param sMaxGuests Maximum number of guests as string, error if blank.
+	 * @param photoURL URL to the house photo, error if blank.
+	 * @param city City in which the house is located, error if blank.
+	 * @param sPrice House price, error if blank.
+	 * @param sStartDate Date in which the city starts being available in format MM/dd/yyyy, error if blank.
+	 * @param sEndDate Date in which the city ends being available in format MM/dd/yyyy, error if blank.
+	 * @return Error message if error, null otherwise.
+	 */
+	public static String checkHouseParameters(String houseName, String fullDescription, String shortDescription,
+			String type, String sGuestNum, String photoURL, String city, String sPrice,
+			String sStartDate, String sEndDate) {
+		String errorMessage  = null;
+		if(houseName == null || houseName.isEmpty()){
+			errorMessage = "Error in house name";
+		}else if(fullDescription == null ||fullDescription.isEmpty()){
+			errorMessage = "Error in full description";
+		}else if(shortDescription == null || shortDescription.isEmpty()){
+			errorMessage = "Error in short description";
+		}else if (type == null || type.isEmpty()){
+			errorMessage = "Error in type";
+		}else if(sGuestNum == null || sGuestNum.isEmpty()){
+			errorMessage = "Error in guest number2";
+		}else if(photoURL == null || photoURL.isEmpty()){
+			errorMessage = "Error in photo url";
+		}else if(city == null || city.isEmpty()){
+			errorMessage = "Error in city";
+		}else if(sPrice == null || sPrice.isEmpty()){
+			errorMessage = "Error in price1";
+		}else if(sStartDate == null || sStartDate.isEmpty()){
+			errorMessage = "Error in start date";
+		}else if(sEndDate == null || sEndDate.isEmpty()){
+			errorMessage = "Error in end date";
+		}
+		return errorMessage;
+	}
+	
+	/**
 	 * Check if the input user parameters are valid in order to sign up a new user.
 	 *
 	 * @param name Name of the user we want to register, error if blank.
